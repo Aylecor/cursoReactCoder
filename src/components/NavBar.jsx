ebc8d1f23f73c7695fa038eedd1c7842c0be73b1
unknown <ayleencordisco@gmail.com>
2022-05-02 22:20:23 -0300

    desafioDos

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import {Navbar} from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
+import {Nav} from 'react-bootstrap'
+import {NavDropdown} from 'react-bootstrap'
+import {Form} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+import {FormControl} from 'react-bootstrap'
+
+const NavBar = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">Real Madrid</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="#action1">Inicio</Nav.Link>
+                    <Nav.Link href="#action2">Historia</Nav.Link>
+                    <NavDropdown title="Link" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Estadio Bernabéu</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Equipo de Fútbol</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#" disabled>
+                    Contacto
+                    </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                    <FormControl
+                    type="search"
+                    placeholder="Buscar"
+                    className="me-2"
+                    aria-label="Search"
+                    />
+                    <Button variant="outline-success">Buscar</Button>
+                </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
